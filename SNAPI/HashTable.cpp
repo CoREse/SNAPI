1fcc4977ccc945cf49203426134ba944339aaba2
CoREse <zzdzhangzzd@hotmail.com>
2015-05-21 23:44:47 +0800

    Complete the HashTable and add Genome

--- SNAPI/HashTable.cpp
+++ SNAPI/HashTable.cpp
@@ -1,12 +1,99 @@
 #include "HashTable.h"
 #include <stdlib.h>
 
+#define DEPT_OF_DETECTION 6
+
+double HashTable::OverflowPara = 0.1;
+unsigned long long HashTable::UnusedKey = 0xffffffffffffffff;
+
 HashTable::HashTable(unsigned long long NumberOfEntries)
 :nMainTable(NumberOfEntries), nOverflowTable(NumberOfEntries*OverflowPara), nUsedMainTable(0), nUsedOverflowTable(0)
 {
-if (NumberOfEntries!=0)
-{
-MainTable=(Entry *)malloc(NumberOfEntries*sizeof(Entry));
-OverflowTable=(OverflowEntry *) malloc(nOverflowTable*sizeof(OverflowEntry));
+	if (NumberOfEntries != 0)
+	{
+		MainTable = (Entry *)malloc(NumberOfEntries*sizeof(Entry));
+		OverflowTable = (OverflowEntry *)malloc(nOverflowTable*sizeof(OverflowEntry));
+	}
+	for (unsigned long long i = 0; i != NumberOfEntries; ++i)
+	{
+		if (i < nOverflowTable)
+		{
+			OverflowTable[i].key = UnusedKey;
+		}
+		MainTable[i].key = UnusedKey;
+	}
 }
+
+HashTable::~HashTable()
+{
+	if (MainTable != nullptr)
+		delete MainTable;
+	if (OverflowTable != nullptr)
+		delete OverflowTable;
+}
+
+bool HashTable::insert(unsigned long long key, unsigned long long location)
+{
+	unsigned long long HashIndex;
+	unsigned detect = 0;
+	while (detect <= DEPT_OF_DETECTION)
+	{
+		HashIndex = Hash(key) % nMainTable + detect*detect;
+		if (MainTable[HashIndex].key == UnusedKey)
+		{
+			MainTable[HashIndex].key = key;
+			MainTable[HashIndex].location = location;
+			return true;
+		}
+		else if (MainTable[HashIndex].key == key)
+		{
+			HashIndex = Hash(key) % nOverflowTable;
+			if (OverflowTable[HashIndex].key == UnusedKey)
+			{
+				OverflowTable[HashIndex].key = key;
+				OverflowTable[HashIndex].locations.push_back(location);
+				return true;
+			}
+			else if (OverflowTable[HashIndex].key == key)
+			{
+				OverflowTable[HashIndex].locations.push_back(location);
+				return true;
+			}
+		}
+		++detect;
+	}
+	return false;
+}
+HashTable::Entry* HashTable::lookupMainTable(unsigned long long key)
+{
+	unsigned long long HashIndex;
+	unsigned detect = 0;
+	while (detect <= DEPT_OF_DETECTION)
+	{
+		HashIndex = Hash(key) % nMainTable + detect*detect;
+		if (MainTable[HashIndex].key == UnusedKey)
+		{
+			return nullptr;
+		}
+		else if (MainTable[HashIndex].key == key)
+		{
+			return MainTable + HashIndex;
+		}
+		++detect;
+	}
+	return nullptr;
+}
+HashTable::OverflowEntry * HashTable::lookupOverflowTable(unsigned long long key)
+{
+	unsigned long long HashIndex = Hash(key) % nOverflowTable;
+	if (OverflowTable[HashIndex].key == key)
+	{
+		return OverflowTable+HashIndex;
+	}
+	return nullptr;
+}
+void HashTable::lookup(unsigned long long key, Entry* MainResult, OverflowEntry* OverflowResult)
+{
+	MainResult = lookupMainTable(key);
+	OverflowResult = lookupOverflowTable(key);
 }
--- SNAPI/HashTable.h
+++ SNAPI/HashTable.h
@@ -1,30 +1,42 @@
 #include <vector>
+//HashTable using square detection
 class HashTable
 {
-Entry *MainTable;
-OverfolwEntry * OverflowTable;
-unsigned long long nMainTable, nOverflowTable, nUsedMainTable, nUsedOverflowTable;
 public:
-static double OverflowPara;
-struct Entry{
-	unsigned long long key;
-	unsigned long long location;	
-};
-struct OverflowEntry
-{
-	unsigned long long key;
-	std::vector<unsigned long long> locations;
-};
-~HashTable();
-HashTable(unsigned long long NumberOfEntries=0);
-inline unsigned long long getnMainTable() const (return nMainTable);
-inline unsigned long long getnUsedMainTable() const (return nUsedMainTable);
-inline unsigned long long getnOverflowTable() const (return nOverflowTable);
-inline unsigned long long getnUsedOverflowTable() const (return nUsedOverflowTable);
+	static double OverflowPara;//nOverflowTable/nMainTable
+	static unsigned long long UnusedKey;
+	static unsigned long long Hash(unsigned long long key)
+	{
+		key ^= key >> 33;
+		key *= 0xff51afd7ed558ccd;
+		key ^= key >> 33;
+		key *= 0xc4ceb9fe1a85ec53;
+		key ^= key >> 33;
+		return key;
+	}
+	struct Entry{
+		unsigned long long key;
+		unsigned long long location;
+	};
+	struct OverflowEntry
+	{
+		unsigned long long key;
+		std::vector<unsigned long long> locations;
+	};
+	~HashTable();
+	HashTable(unsigned long long NumberOfEntries = 0);
+	inline unsigned long long getnMainTable() const { return nMainTable; }
+	inline unsigned long long getnUsedMainTable() const { return nUsedMainTable; }
+	inline unsigned long long getnOverflowTable() const { return nOverflowTable; }
+	inline unsigned long long getnUsedOverflowTable() const { return nUsedOverflowTable; }
 
-bool insert(unsigned long long key, unsigned long long location);
-Entry* lookupMainTable(unsigned long long key);
-OverflowEntry * lookupOverflowTable(unsigned long long key);
-OverflowEntry lookupIntel(unsigned long long) const;
-void lookup(unsigned long long key, Entry* MainResult, Overflow* OverflowResult);
+	bool insert(unsigned long long key, unsigned long long location);
+	Entry* lookupMainTable(unsigned long long key);
+	OverflowEntry * lookupOverflowTable(unsigned long long key);
+	//OverflowEntry lookupIntel(unsigned long long) const;
+	void lookup(unsigned long long key, Entry* MainResult, OverflowEntry* OverflowResult);
+private:
+	Entry *MainTable;
+	OverflowEntry * OverflowTable;
+	unsigned long long nMainTable, nOverflowTable, nUsedMainTable, nUsedOverflowTable;
 };
